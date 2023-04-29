fb6584a061bba8ec7f6e2780370a52001c6d9b99
Vasily Kulikov <azotbitzproduction@gmail.com>
2023-04-29 03:11:38 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, {useEffect, useState} from "react";
+
+
+
+const App = ()  => {
+
+    const [valueSec, setValueSec] = useState('');
+    const [sec, setSec] = useState('')
+    const [timer, setTimer] = useState('hh:mm:ss')
+    const [sendRequest, setSendRequest] = useState(false);
+
+  useEffect(() => {
+      if(sendRequest){
+     const interval = setInterval(() => {
+          setSec(sec => sec - 1)
+         setTimer(timer => timer = convertTime(sec))
+      }, 1000);
+     return () => clearInterval(interval)}
+  }, [sendRequest, timer])
+
+  const convertTime = (time) => {
+    let value = parseInt(time, 10)
+    let hours = Math.floor(value / 3600);
+    let minutes = Math.floor((value - (3600 * hours)) / 60);
+    let seconds = Math.floor((value - (3600 * hours)) - (minutes * 60));
+    if (hours   < 10) {hours = "0" + hours}
+    if (minutes < 10) {minutes = "0" + minutes}
+    if (seconds < 10) {seconds = "0" + seconds}
+    return hours + ':' + minutes + ':' + seconds;
+  }
+    const handleChange = (event) => {
+        const value = event.target.value;
+        setValueSec(value)
+    }
+    const btnAction = () => {
+        setSec(valueSec)
+        setSendRequest(prev => !prev)
+        setValueSec('')
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <input value={valueSec} onChange={handleChange} placeholder="Seconds" type="text" />
+
+        <button onClick={btnAction}>Start</button>
+
+        <br />
+        <br />
+
+        <span>{timer}</span>
+      </>
   );
 }
 
